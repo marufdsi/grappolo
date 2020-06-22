5585a14022d7787b0db2c60a19a58e6f68d3d26d
Md Maruf Hossain <maruf.csea1@gmail.com>
2020-06-22 01:48:57 -0400

    results sets are added

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#export OMP_NUM_THREADS=18
-#export KMP_AFFINITY="explicit,proclist=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34],verbose"
+#export OMP_NUM_THREADS=24
+#export KMP_AFFINITY="explicit,proclist=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46],verbose"
 
 dr="datasets"
 f=5
-for g in "Cage15.graph" "uk2002.graph" "333SP.graph" "AS365.graph" "eu-2005.graph" "in-2004.graph" "M6.graph" "NLR.graph" "road_usa.graph" "af_shell10.graph" "cnr-2000.graph" "G_n_pin_pout.graph" "NACA0015.graph" "road_central.graph" "smallworld.graph"
+for g in "cage15.graph" "uk2002.graph" "333SP.graph" "AS365.graph" "eu-2005.graph" "in-2004.graph" "M6.graph" "NLR.graph" "road_usa.graph" "af_shell10.graph" "cnr-2000.graph" "G_n_pin_pout.graph" "NACA0015.graph" "road_central.graph" "smallworld.graph"
 do
     ./driverForGraphClustering -f $f  $dr"/"$g -b 0
 done
