68d69f91ce7a7c528ad41fec97b8b9af582f6c80
Maruf <maruf.csea1@gmail.com>
2020-06-26 06:28:58 -0400

    OpenMp SpMV updated

--- Utility/utilityClusteringFunctions.cpp
+++ Utility/utilityClusteringFunctions.cpp
@@ -526,6 +526,7 @@ f_weight buildLocalMapCounterVec_SFP(comm_type v, comm_type *cid, f_weight *Coun
     comm_type storedAlready = 0;
     f_weight selfLoop = 0;
     comm_type vector_op = (adj2-adj1)/16;
+    cout << "Neighbors: " << adj2-adj1 << endl;
     /// perform intrinsic on the neighbors that are multiple of 16
     const   __m512i check_self_loop = _mm512_set1_epi32(v);
 //    cout << "Perform vector operation" << endl;
@@ -601,6 +602,7 @@ f_weight buildLocalMapCounterVec_SFP(comm_type v, comm_type *cid, f_weight *Coun
             numUniqueClusters++;
         }
     }//End of for(j)
+    cout << "numUniqueClusters: " << numUniqueClusters << endl;
     return selfLoop;
 }//End of buildLocalMapCounter()
                                                                                 
