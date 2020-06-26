66c17a5c7eec5750d783f1eaabc664b9681c9938
Maruf <maruf.csea1@gmail.com>
2020-06-26 04:33:45 -0400

    OpenMp SpMV updated

--- DefineStructure/basic_util.h
+++ DefineStructure/basic_util.h
@@ -14,6 +14,8 @@ inline void Visit(comm_type v, comm_type myCommunity, short *Visited, comm_type 
                   comm_type* vtxPtr, edge* vtxInd, comm_type *C);
 inline void Visit(comm_type v, comm_type myCommunity, comm_type *Visited, comm_type *Volts,
                   comm_type* vtxPtr, edge* vtxInd, comm_type *C);
+inline void Visit(comm_type v, comm_type myCommunity, comm_type *Visited, long *Volts,
+                  comm_type* vtxPtr, edge* vtxInd, long *C);
 				  
 // Define in vertexFollowing.cpp
 long vertexFollowing(graph *G, long *C);
