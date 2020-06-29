ad418a1e1af64d45f62476ade5641b9ca25e869f
Maruf <maruf.csea1@gmail.com>
2020-06-29 03:42:01 -0400

    OpenMp SpMV updated

--- BasicCommunitiesDetection/vectorizedLouvainMethod.cpp
+++ BasicCommunitiesDetection/vectorizedLouvainMethod.cpp
@@ -414,8 +414,8 @@ f_weight vectorizedLouvianMethod(graph *G, comm_type *C, int nThreads, f_weight 
 
     /// pointer to track existing community
     cout << "Error check pint " << endl;
-    comm_type** track_cid;
-    posix_memalign((void **) track_cid, alignment, nThreads * sizeof(comm_type *));
+    comm_type** track_cid = (comm_type **) malloc (nThreads * sizeof(comm_type *));
+//    posix_memalign((void **) track_cid, alignment, nThreads * sizeof(comm_type *));
     assert(track_cid != 0);
     for (int k = 0; k < nThreads; ++k) {
         posix_memalign((void **) &track_cid[k], alignment, NV * sizeof(comm_type));
